a04771886b3d92718f642ad8db0ec2c5aae7214c
linhdoan9513 <78831085+linhdoan9513@users.noreply.github.com>
2021-09-03 10:38:47 -0400

    "updated Sep 3"

---
+++ squareOfSortedArray.js
@@ -0,0 +1,8 @@
+//https://leetcode.com/problems/squares-of-a-sorted-array/
+var sortedSquares = function (nums) {
+  let arr = [];
+  for (let i = 0; i < nums.length; i++) {
+    arr.push(nums[i] * nums[i]);
+  }
+  return arr.sort((a, b) => a - b);
+};
