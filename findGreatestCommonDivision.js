a54bc188a477004f1c4144fce8c89a28deb85115
linhdoan9513 <78831085+linhdoan9513@users.noreply.github.com>
2021-09-13 15:36:56 -0400

    "updated time complexity"

--- findGreatestCommonDivision.js
+++ findGreatestCommonDivision.js
@@ -1,4 +1,6 @@
 //https://leetcode.com/problems/find-greatest-common-divisor-of-array/
+
+//Time Complexity: O(n log n) and space complexity O(1);
 var findGCD = function (nums) {
   let minNum = Math.min(...nums);
   let maxNum = Math.max(...nums);
