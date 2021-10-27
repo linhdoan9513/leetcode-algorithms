b0561c0b485887be24551f5a44582f2a1b77e698
linhdoan9513 <78831085+linhdoan9513@users.noreply.github.com>
2021-10-27 11:46:45 -0400

    leetcode - easy 1086

---
+++ highFive.js
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/high-five/
+
+var highFive = function (items) {
+  let table = {};
+  let results = [];
+
+  for (let i = 0; i < items.length; i++) {
+    if (!(items[i][0] in table)) {
+      table[items[i][0]] = [items[i][1]];
+    } else {
+      table[items[i][0]].push(items[i][1]);
+    }
+  }
+
+  for (const grade in table) {
+    let topGrade = table[grade];
+    topGrade = topGrade.sort((a, b) => b - a);
+    let average = 0;
+
+    for (let i = 0; i < 5; i++) {
+      average += table[grade][i];
+    }
+
+    average = Math.floor(average / 5);
+    results.push([grade, average]);
+  }
+
+  return results;
+};
